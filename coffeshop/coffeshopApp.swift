8432a6122011c9d0dc14718119f597ac8b1c3752
Garfianto <fianto74@gmail.com>
2022-11-19 21:18:06 +0700

    Initial Commit

---
+++ coffeshop/coffeshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeshopApp.swift
+//  coffeshop
+//
+//  Created by Garfianto Dwi Cahyadi on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct coffeshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
